e8dbb96e98110eb4ff8b2c4113433162f13212fc
Shehzad Raheem <shehzadraheem.sr38@gmail.com>
2023-12-26 21:24:02 +0500

    update_port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
