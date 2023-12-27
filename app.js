4ab1753c1a355e50a78279556a43ebef10e3cb2b
Shehzad Raheem <shehzadraheem.sr38@gmail.com>
2023-12-27 21:56:41 +0500

    small_changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
@@ -14,18 +14,17 @@ app.use('/graphql', graphqlHTTP({
     schema: schema
 }))
 
-// app.listen(4000, ()=> {
-//     console.log('Hello');
-// });
+
 
 mongoose.connect( `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.fpcxm.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen({ port: port }, () => {
+    console.log('Hello');
+    app.listen(PORT, () => {
       //console.log(process.env.mongoUserName);
       //localhost:4000
       console.log("Listening for requests on my awesome port " + port);
     });
   }).catch((e) => {
     //console.log(process.env.mongoUserName);
-    return console.log("Error:::" + e);
+     console.log("Error Found:::" + e);
   });
