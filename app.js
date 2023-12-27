26f658031cc038fa89d204bf1c450a9cd48934f1
Shehzad Raheem <shehzadraheem.sr38@gmail.com>
2023-12-27 22:14:29 +0500

    update

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-// const express = require('express');
-// const { graphqlHTTP } = require('express-graphql');
-// const schema = require('./server/schema/schema');
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require('dotenv').config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./server/schema/schema');
+const mongoose = require("mongoose");
+const cors = require("cors");
+require('dotenv').config();
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// app.use(cors());
-// app.use('/graphql', graphqlHTTP({
-//     graphiql: true,
-//     schema: schema
-// }))
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
 
 
 
-// mongoose.connect( `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.fpcxm.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`)
-//   .then(() => {
-//     console.log('Hello');
-//     app.listen(PORT, () => {
-//       //console.log(process.env.mongoUserName);
-//       //localhost:4000
-//       console.log("Listening for requests on my awesome port " + port);
-//     });
-//   }).catch((e) => {
-//     //console.log(process.env.mongoUserName);
-//      console.log("Error Found:::" + e);
-//   });
-
-const express = require('express')
-const mongoose = require('mongoose')
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
-
-//Routes go here
-app.all('*', (req,res) => {
-    res.json({"every thing":"is awesome"})
-})
-
-//Connect to the database before listening
-connectDB().then(() => {
+mongoose.connect( `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.fpcxm.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log('Hello');
     app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-})
+      //console.log(process.env.mongoUserName);
+      //localhost:4000
+      console.log("Listening for requests on my awesome port " + port);
+    });
+  }).catch((e) => {
+    //console.log(process.env.mongoUserName);
+     console.log("Error Found:::" + e);
+  });
+
+// const express = require('express')
+// const mongoose = require('mongoose')
+
+// const app = express()
+// const PORT = process.env.PORT || 3000
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// }
+
+// //Routes go here
+// app.all('*', (req,res) => {
+//     res.json({"every thing":"is awesome"})
+// })
+
+// //Connect to the database before listening
+// connectDB().then(() => {
+//     app.listen(PORT, () => {
+//         console.log("listening for requests");
+//     })
+// })
